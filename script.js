e09c1e90c5bd38ac436b2e7162121008de246248
gerardasanusevicius <gerard.a.official@gmail.com>
2022-02-02 23:38:30 +0200

    midway

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+// TABS
+function changeContent(index,id){
+    var contents = document.querySelectorAll(".content");
+    var tabButtons = document.querySelectorAll(".buttontab");
+
+    for(let i = 0; i < contents.length; i++){
+        contents[i].classList.remove("display");
+        contents[i].classList.remove("transform");
+        tabButtons[i].classList.remove("active");
+    }
+
+    var element = document.getElementById(id);
+    element.classList.add("display");
+    tabButtons[index].classList.add("active");
+    
+
+    setTimeout(function(){
+        element.classList.add("transform");
+    },100);
+}
