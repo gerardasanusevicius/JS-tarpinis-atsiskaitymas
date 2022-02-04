ce2593b86a044dbdeccb03c62d729a2accc90f8c
gerardasanusevicius <gerard.a.official@gmail.com>
2022-02-04 17:34:38 +0200

    almostdonewithoutswipermedia

--- script.js
+++ script.js
@@ -25,14 +25,48 @@ function changeContent(index,id){
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'horizontal',
+   direction: 'horizontal',
   slidesPerView: 3,
   spaceBetween: 20,
   loop: true,
   speed: 1000,
+  breakpoints: {
+
+      720: {
+          slidesperView: 2,
+          slidesPerGroup: 2,
+          allowSlideNext: true,
+          allowSlidePrev: true,
+      },
+
+      1280: {
+          slidesperView: 3,
+          slidesPerGroup: 3,
+          allowSlideNext: true,
+          allowSlidePrev: true,
+      }
+
+    },
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
 });
+
+// BURGER MENU
+
+var burger = document.getElementById("burger");
+var sideMenu = document.getElementById("side-menu");
+
+burger.addEventListener("click", function(){
+    sideMenu.classList.toggle("width");
+    this.classList.toggle("rotateZ");
+});
+
+window.addEventListener("resize",function(){
+    if(window.innerWidth > 760) {
+        sideMenu.classList.remove("width");
+        burger.classList.remove("rotateZ");
+    }
+})
